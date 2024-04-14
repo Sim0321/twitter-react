f01e9db9e1610a687b0bcd4d1b9e09acb0e643c4
Sim <0doogadooga@gmail.com>
2024-04-14 15:00:39 +0900

    #2 firebase configure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import Router from "pages/components/Router";
 import { Layout } from "pages/components/Layout";
 
+import { getAuth } from "firebase/auth";
+import { app } from "firebaseApp";
+import { useState } from "react";
+
 function App() {
+  const auth = getAuth(app);
+
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(
+    !!auth?.currentUser
+  ); // currentUser 초기값은 null이라 true,false로 로그인 상태인지 로그아웃 상태인지 판별하기 위함
+
   return (
     <Layout>
       <Router />
