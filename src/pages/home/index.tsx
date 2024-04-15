32da82c32d289076faae69f9fc87c1603bbf99a5
Sim <0doogadooga@gmail.com>
2024-04-15 21:27:06 +0900

    #1 header 고정 적용

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -68,12 +68,15 @@ const posts: PostProps[] = [
 export default function HomePage() {
   return (
     <div className="home">
-      <div className="home__title">Home</div>
-      <div className="home__tabs">
-        <div className="home__tab home__tab--active">For You</div>
-        <div className="home__tab">Following</div>
+      <div className="home__top">
+        <div className="home__title">Home</div>
+        <div className="home__tabs">
+          <div className="home__tab home__tab--active">For You</div>
+          <div className="home__tab">Following</div>
+        </div>
+        <PostForm />
       </div>
-      <PostForm />
+
       {/* tweet posts */}
       <div className="post">
         {posts?.map((post) => (
