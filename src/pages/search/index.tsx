36cdb23f4e5cf0037556b8fc17896d62ff0d7e51
Sim <0doogadooga@gmail.com>
2024-04-18 23:14:34 +0900

    #3 hashtag 검색 후 리스트 조회

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,3 +1,70 @@
+import PostBox from "components/posts/PostBox";
+import AuthContext from "context/AuthContext";
+import {
+  collection,
+  orderBy,
+  query,
+  where,
+  onSnapshot,
+} from "firebase/firestore";
+import { db } from "firebaseApp";
+import { PostProps } from "pages/home";
+import { useContext, useEffect, useState } from "react";
+
 export default function SearchPage() {
-  return <h1>SearchPage</h1>;
+  const { user } = useContext(AuthContext);
+  const [posts, setPosts] = useState<PostProps[]>([]);
+  const [tagQuery, setTagQuery] = useState<string>("");
+  const onChange = (e: any) => {
+    setTagQuery(e.target.value.trim());
+  };
+
+  useEffect(() => {
+    if (user) {
+      let postsRef = collection(db, "posts");
+      let postQuery = query(
+        postsRef,
+        where("hashTags", "array-contains-any", [tagQuery]),
+        orderBy("createdAt", "desc")
+      );
+
+      onSnapshot(postQuery, (snapshot) => {
+        let dataObj = snapshot.docs?.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        }));
+
+        setPosts(dataObj as PostProps[]);
+      });
+    }
+  }, [tagQuery, user]);
+
+  return (
+    <div className="home">
+      <div className="home__top">
+        <div className="home__title">
+          <div className="home__title-text">Search</div>
+        </div>
+
+        <div className="home__search-div">
+          <input
+            type="text"
+            className="home__search"
+            placeholder="해시태그 검색"
+            onChange={onChange}
+          />
+        </div>
+
+        <div className="post">
+          {posts?.length > 0 ? (
+            posts?.map((post) => <PostBox post={post} key={post.id} />)
+          ) : (
+            <div className="post__no-posts">
+              <div className="post__text">게시글이 없습니다.</div>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
