f4cfab57e4e646bc4fab84b1f705cd919af09aea
Sim <0doogadooga@gmail.com>
2024-05-04 16:42:01 +0900

    #7 댓글 기능 완료

--- src/pages/posts/detail.tsx
+++ src/pages/posts/detail.tsx
@@ -3,9 +3,11 @@ import { PostProps } from "./../home/index";
 import PostBox from "components/posts/PostBox";
 import Loader from "components/loader/Loader";
 import { useParams } from "react-router-dom";
-import { doc, getDoc } from "firebase/firestore";
+import { doc, getDoc, onSnapshot } from "firebase/firestore";
 import { db } from "firebaseApp";
 import PostHeader from "components/posts/Header";
+import CommentForm from "components/comments/CommentForm";
+import CommentBox, { CommentProps } from "components/comments/CommentBox";
 
 export default function PostDetailPage() {
   const params = useParams();
@@ -14,9 +16,10 @@ export default function PostDetailPage() {
   const getPost = useCallback(async () => {
     if (params.id) {
       const docRef = doc(db, "posts", params.id);
-      const docSnap = await getDoc(docRef);
 
-      setPost({ ...(docSnap?.data() as PostProps), id: docSnap.id });
+      onSnapshot(docRef, (doc) => {
+        setPost({ ...(doc?.data() as PostProps), id: doc.id });
+      });
     }
   }, [params.id]);
 
@@ -27,7 +30,20 @@ export default function PostDetailPage() {
   return (
     <div className="post">
       <PostHeader />
-      {post ? <PostBox post={post} /> : <Loader />}
+      {post ? (
+        <>
+          <PostBox post={post} />
+          <CommentForm post={post} />
+          {post?.comments
+            ?.slice(0)
+            ?.reverse()
+            ?.map((data: CommentProps, index: number) => (
+              <CommentBox data={data} key={index} post={post} />
+            ))}
+        </>
+      ) : (
+        <Loader />
+      )}
     </div>
   );
 }
